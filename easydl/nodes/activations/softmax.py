Fixing softmax implementation.

easydl/nodes/activations/softmax.py
@@ -15,25 +15,27 @@ class Softmax(Activation):
         check_arg_number(inputs, 1)
 
     def forward(self, inputs: Union[np.ndarray, List[np.ndarray]], batch_size: int):
-
         # inputs x
+
+        input_size = inputs[0].shape[1]
+        add_ones = self.np.ones((input_size, 1))
+
         exp = self.np.exp(inputs[0])  # f
 
-        exp_sum = (self.np.sum(exp, 1) + self.eps).reshape((-1, 1))  # g and h=exp
+        exp_sum = (np.dot(exp, add_ones) + self.eps).reshape((-1, 1))  # g and h=exp
 
         sm = exp / exp_sum  # y
 
-        return sm, (exp, exp_sum, sm)
+        return sm, (exp, exp_sum, add_ones)
 
     def backward(self, gradients: np.ndarray, cache: Union[None, np.ndarray, List[np.ndarray]], batch_size):
-        exp, exp_sum, sm = cache
+        exp, exp_sum, add_ones = cache
 
-        d_y_d_f = gradients / (exp + self.eps)
-        d_y_d_x = d_y_d_f
+        d_y_d_x = self.np.multiply(gradients, exp) / exp_sum
 
-        d_y_d_g = self.np.dot(exp + gradients, -self.np.ones((sm.shape[1], 1))) / (self.np.power(exp_sum, 2) + self.eps)
-        d_y_d_h = self.np.dot(d_y_d_g, self.np.ones((1, exp.shape[1])))
-        d_h_d_x = d_y_d_h
+        d_y_d_g = self.np.dot(self.np.multiply(exp, gradients), -add_ones) / self.np.power(exp_sum, 2)
+        d_y_d_h = self.np.dot(d_y_d_g, add_ones.T)
+        d_h_d_x = self.np.multiply(d_y_d_h, exp)
 
         d_y_d_x = d_y_d_x + d_h_d_x
 

easydl/nodes/constant.py
@@ -7,4 +7,4 @@ from easydl.abstract_tensor import AbstractTensor
 
 class Constant(AbstractTensor):
     def __init__(self, array: np.ndarray):
-        super().__init__(array, False)
+        super().__init__(array, None)

easydl/tensor.py
@@ -3,7 +3,7 @@ import numpy as np
 from typing import Union
 from .abstract_tensor import AbstractTensor
 from .nodes.operations import Add, Substract, Multiply, Divide, Power
-from easydl.variables.constant import Constant
+from .nodes.constant import Constant
 from .util.input_check import check_equal_shape
 
 
@@ -14,7 +14,7 @@ class tensor(AbstractTensor):
 
     def base_op_two_args(self, op, other: Union[float, tensor], tmp_first: bool = False):
         if isinstance(other, (float, int)):
-            constant = np.ones_like(self.numpy) * other
+            constant = self.np.ones_like(self.numpy) * other
             tmp_tensor = Constant(constant)
         else:
             check_equal_shape([other.numpy, self.numpy])

entry.py
@@ -16,7 +16,7 @@ def eval_func(data, label):
     for i in range(0, data.shape[0], batch_size):
         target = tensor(label[i:batch_size + i])
         source = tensor(data[i:batch_size + i])
-        d = re(l2(re(l(source))))
+        d = so(l2(re(l(source))))
 
         pred = np.argmax(d.numpy)
         true_pred = target.numpy.flatten()
@@ -28,29 +28,29 @@ def eval_func(data, label):
 
 def test_func(data, label):
 
-    for e in range(30):
+    for e in range(5):
         batch_size = 4
         for i in range(0, data.shape[0], batch_size):
 
             target = tensor(label[i:batch_size+i])
             source = tensor(data[i:batch_size+i])
 
-            #with Tape() as tape:
-            d = s(l2(re(l(source))))
+            with Tape() as tape:
+                d = so(l2(re(l(source))))
 
-            # d.to_gpu()
-            # target.to_gpu()
-            # mse.to_gpu()
+                # d.to_gpu()
+                # target.to_gpu()
+                # mse.to_gpu()
 
-            r = mse([d, target])
-            print(np.sum(r.numpy))
+                r = mse([d, target])
+                print(np.sum(r.numpy))
 
             r.backward()
-            #optimizer.optimize(tape)
+            optimizer.optimize(tape)
 
 
 edl.init_easydl(True)
-optimizer = Sgd(learning_rate=0.6, momentum=0.1)
+optimizer = Sgd(learning_rate=0.03, momentum=0.1)
 
 l = Dense(784, 128)
 #l.to_gpu()